7caa920c0d6efeb361aed812ac9a26c639aed5c8
Daniel Romero <dromero@alumnos.inf.utfsm.cl>
2013-11-25 06:51:23 -0300

    Mejorado: Tabs , faltando solo perfil Falta arreglar pop de mensajes, busca “Model”

---
+++ js/bootstrap-modal.js
@@ -0,0 +1,13 @@
+$('#openBtn').click(function(){
+  	$('#myModal').modal('show');
+});
+
+$('#toggleBtn').click(function(){
+  	$('#myModal').modal($('#openBtn').click(function(){
+	$('#myModal').modal({show:true})
+});'toggle');
+});
+
+$('#myModal').on('hidden', function () {
+  document.location.reload();
+})
